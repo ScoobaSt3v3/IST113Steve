9a30ffd2835f6f05800cece88cbf4d56ec82f8e6
ScoobaSt3v3 <39249810+ScoobaSt3v3@users.noreply.github.com>
2018-08-20 15:01:03 -0400

    added link and calc file

---
+++ jsCalc.js
@@ -0,0 +1,25 @@
+
+
+function add(number1, number2) {
+    return(number1 + number2);
+}
+
+function subtract(number1, number2) {
+    return(number1 - number2);
+}
+
+function multiply(number1, number2) {
+    return(number1 * number2);
+}
+
+function divide(number1, number2) {
+    return(number1 / number2);
+}
+
+function testCalc() {
+    console.log(add(10, 2));
+    console.log(subtract(8, 5));
+    console.log(multiply(4, 4));
+    console.log(divide(8, 4));
+
+}
