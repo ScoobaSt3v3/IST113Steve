c4ff4de8bd56122c841d5a9c75a4a424fa2746e0
ScoobaSt3v3 <39249810+ScoobaSt3v3@users.noreply.github.com>
2018-08-20 20:06:04 -0400

    f

--- jsCalc/jsCalc.js
+++ jsCalc/jsCalc.js
@@ -16,7 +16,7 @@ function divide(number1, number2) {
     return(number1 / number2);
 }
 
-document.getElementById.addEventListener("click", add(2, 4));
+document.getElementById.("calcAdd).innerHTML = add(2, 4);
 document.getElementById("calcSubtract") = subtract(2, 4);
 document.getElementById("calcMultiply") = multiply(2, 4);
 document.getElementById("calcDivide") = divide(2, 4);
