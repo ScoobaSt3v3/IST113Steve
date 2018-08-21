f1a0fecfed8d478e425ee8c64910b7fbb4a75eb4
ScoobaSt3v3 <39249810+ScoobaSt3v3@users.noreply.github.com>
2018-08-20 20:06:47 -0400

    f

--- jsCalc/jsCalc.js
+++ jsCalc/jsCalc.js
@@ -20,10 +20,3 @@ document.getElementById.("calcAdd).innerHTML = add(2, 4);
 document.getElementById("calcSubtract") = subtract(2, 4);
 document.getElementById("calcMultiply") = multiply(2, 4);
 document.getElementById("calcDivide") = divide(2, 4);
-/* function testCalc() {
-	console.log(add(10, 2));
-    console.log(subtract(8, 5));
-    console.log(multiply(4, 4));
-    console.log(divide(8, 4));
-
-} *\
